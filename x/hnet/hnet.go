9ca1cddffc821003aea76c5e6fadacb8731cf5dc
periaate <daniel.saury.00@gmail.com>
2024-11-21 04:00:35 +0200

    cors

--- x/hnet/hnet.go
+++ x/hnet/hnet.go
@@ -17,35 +17,35 @@ func Log(next http.Handler) http.Handler {
 }
 
 const (
-	ACA_ORIGIN      = "Access-Control-Allow-Origin"
-	ACA_METHODS     = "Access-Control-Allow-Methods"
-	ACA_HEADERS     = "Access-Control-Allow-Headers"
-	ACA_CREDENTIALS = "Access-Control-Allow-Credentials"
+	ACCESSS_CONTROL_ALLOW_ORIGIN      = "Access-Control-Allow-Origin"
+	ACCESSS_CONTROL_ALLOW_METHODS     = "Access-Control-Allow-Methods"
+	ACCESSS_CONTROL_ALLOW_HEADERS     = "Access-Control-Allow-Headers"
+	ACCESSS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials"
 
-	DEF_ACA_ORIGIN      = "*"
-	DEF_ACA_METHODS     = "GET, POST, PUT, DELETE, OPTIONS"
-	DEF_ACA_HEADERS     = "Content-Type, Authorization"
-	DEF_ACA_CREDENTIALS = "true"
+	DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN      = "*"
+	DEFAULT_ACCESS_CONTROL_ALLOW_METHODS     = "GET, POST, PUT, DELETE, OPTIONS"
+	DEFAULT_ACCESS_CONTROL_ALLOW_HEADERS     = "Content-Type, Authorization"
+	DEFAULT_ACCESS_CONTROL_ALLOW_CREDENTIALS = "true"
 )
 
 type CORS struct {
-	ACA_Origin      string
-	ACA_Methods     string
-	ACA_Headers     string
-	ACA_Credentials string
+	ACCESS_CONTROL_ALLOW_ORIGIN      string
+	ACCESS_CONTROL_ALLOW_METHODS     string
+	ACCESS_CONTROL_ALLOW_HEADERS     string
+	ACCESS_CONTROL_ALLOW_CREDENTIALS string
 }
 
 func (c CORS) Handler(next http.Handler) http.Handler {
-	acao := gen.Or(c.ACA_Origin, DEF_ACA_ORIGIN)
-	acam := gen.Or(c.ACA_Methods, DEF_ACA_METHODS)
-	acah := gen.Or(c.ACA_Headers, DEF_ACA_HEADERS)
-	acac := gen.Or(c.ACA_Credentials, DEF_ACA_CREDENTIALS)
+	ACCESS_CONTROL_ALLOW_ORIGIN_VALUE := gen.Or(c.ACCESS_CONTROL_ALLOW_ORIGIN, DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN)
+	ACCESS_CONTROL_ALLOW_METHODS_VALUE := gen.Or(c.ACCESS_CONTROL_ALLOW_METHODS, DEFAULT_ACCESS_CONTROL_ALLOW_METHODS)
+	ACCESS_CONTROL_ALLOW_HEADERS_VALUE := gen.Or(c.ACCESS_CONTROL_ALLOW_HEADERS, DEFAULT_ACCESS_CONTROL_ALLOW_HEADERS)
+	ACCESS_CONTROL_ALLOW_CREDENTIALS_VALUE := gen.Or(c.ACCESS_CONTROL_ALLOW_CREDENTIALS, DEFAULT_ACCESS_CONTROL_ALLOW_CREDENTIALS)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set(ACA_ORIGIN, acao)
-		w.Header().Set(ACA_ORIGIN, acam)
-		w.Header().Set(ACA_ORIGIN, acah)
-		w.Header().Set(ACA_ORIGIN, acac)
+		w.Header().Set(ACCESSS_CONTROL_ALLOW_ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN_VALUE)
+		w.Header().Set(ACCESSS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_METHODS_VALUE)
+		w.Header().Set(ACCESSS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_HEADERS_VALUE)
+		w.Header().Set(ACCESSS_CONTROL_ALLOW_CREDENTIALS, ACCESS_CONTROL_ALLOW_CREDENTIALS_VALUE)
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
