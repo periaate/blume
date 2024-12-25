313aa8ffc88f4c316cefd91fb36a569b7649cb19
periaate <daniel@saury.fi>
2024-12-25 07:13:59 +0200

    uhhh im tired

--- cmd/filter/main_test.go
+++ cmd/filter/main_test.go
@@ -1,51 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 
 	. "github.com/periaate/blume"
 )
 
-func Expect(t *testing.T) func(fn func([]string) bool) func(values []string) []string {
-	return func(fn func([]string) bool) func(values []string) []string {
-		return func(values []string) []string {
-			if !fn(values) {
+func TestFilter(t *testing.T) {
+	args := []string{
+		"a", "b",
+		"A", "B",
+		"abc", "bca", "cab",
+		"ABC", "BCA", "CAB",
+		"ababc", "baabc", "abbac", "cabba", "cbaab",
+		"ABABC", "BAABC", "ABBAC", "CABBA", "CBAAB",
+	}
+
+	exps := []struct {
+		pred  func(string) bool
+		input []string
+	}{
+		{Is("a"), []string{"is", "a"}},
+		{Contains("ba"), []string{"has", "ll"}},
+		{HasPrefix("a"), []string{"pre", "a"}},
+		{HasPrefix("ab"), []string{"pre", "ab"}},
+	}
+
+	for _, run := range exps {
+		t.Run(strings.Join(run.input, " "), func(t *testing.T) {
+			res := Parse(run.input)(args)
+			if len(res) == 0 {
 				t.Fail()
 			}
-			return values
-		}
-	}
-}
-
-func For[A any](fn func(A)) func([]A) {
-	return func(arr []A) {
-		for _, v := range arr {
-			fn(v)
-		}
-	}
-}
-
-// Just is composition of [All] and [Is].
-func Just[C comparable](A ...C) func([]C) bool { return All(Is(A...)) }
-func Ignore[A, B any](fn func(A) B) func(A)    { return func(a A) { fn(a) } }
-
-func After[A, B, C any](fn func(A) func(B) C) func(after func(C)) func(A) func(B) C {
-	return func(after func(C)) func(A) func(B) C {
-		return func(a A) func(B) C {
-			return func(b B) C {
-				res := fn(a)(b)
-				after(res)
-				return res
+			if !All(run.pred)(res) {
+				t.Fail()
 			}
-		}
+		})
 	}
 }
-
-func TestFilter(t *testing.T) {
-	expect := After(Expect(t))(For(func(str string) { fmt.Println(str) }))
-	After(Parse)(Ignore(expect(Just("hello"))))(Join("is", "hello"))(Join("Hello", "World", "hello", "world"))
-	// expect(Just("hello"))(Parse("is", "hello")("Hello", "World", "hello", "world"))
-}
-
-func Join[A any](args ...A) []A { return args }
